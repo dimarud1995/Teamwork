b587d4682454e4772c0bbda63cfaac0a1b602f9d
inform304@gmail.com <inform304@gmail.com>
2015-11-02 18:17:20 +0200

    edit sms model

--- SVTrade/Models/SMS.cs
+++ SVTrade/Models/SMS.cs
@@ -8,6 +8,7 @@ namespace SVTrade.Models
         [DisplayName("Номер, кому призначається СМС(Приклад: 380993334455)")]
         [Required(ErrorMessage = "Введіть номер телефону", AllowEmptyStrings = false)]
         [RegularExpression(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$", ErrorMessage = "Неправильний формат номера телефону")]
+
         public string Numbers { get; set; }
 
         [DisplayName("Від кого відправляється СМС(Приклад: SVTRADE)")]
